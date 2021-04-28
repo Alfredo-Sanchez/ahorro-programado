9c553228f797fda5df2d5e497a6bef5ef0897cff
Alfredo Sanchez <alfredosanchez080114@gmail.com>
2021-04-28 11:56:14 -0400

    tooltip thousand separator

--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,7 @@ function totalCaseChart(ctx){
                     borderColor: "rgb(75, 192, 192)",
                     borderWidth: 1,
                     data: ahorroGlobal
-                },
+                }
             
             ]
         },
@@ -73,7 +73,7 @@ function totalCaseChart(ctx){
              yAxes: [{
                 ticks: {
                     beginAtZero: true,
-                    stepSize: 500000,
+                    stepSize: (ahorroGlobal * 60)/10,
             
                     // Return an empty string to draw the tick line but hide the tick label
                     // Return `null` or `undefined` to hide the tick line entirely
@@ -96,13 +96,38 @@ function totalCaseChart(ctx){
                 padding: 30,
                 fontColor: '#000'
             },
+            tooltips: {
+                callbacks: {
+                  // this callback is used to create the tooltip label
+                  label: function(tooltipItem, data) {
+                    // get the data label and data value to display
+                    // convert the data value to local string so it uses a comma seperated number
+                    var dataLabel = data.labels[tooltipItem.index];
+                    var value = ': ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
+          
+                    // make this isn't a multi-line label (e.g. [["label 1 - line 1, "line 2, ], [etc...]])
+                    if (Chart.helpers.isArray(dataLabel)) {
+                      // show value on first line of multiline label
+                      // need to clone because we are changing the value
+                      dataLabel = dataLabel.slice();
+                      dataLabel[0] += value;
+                    } else {
+                      dataLabel += value;
+                    }
+          
+                    // return the text to display on the tooltip
+                    return dataLabel;
+                  }
+                }
+            },
             layout:{
                  padding:{
                     right: 10,
                     left: 10,
                     bottom: 50
                  }
-             }
+            }
+             
         }
         
     });
